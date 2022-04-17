d2c7c03ec262d49bdc5045720f118a27a27f9b3b
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-04-17 22:58:26 +0300

    addTodoList

---
+++ js/app.js
@@ -0,0 +1,34 @@
+alert("Hello! I am an alert box!");
+
+var name = prompt("Inter Your Name");
+
+var gender = prompt("Inter Your Gender (male or female)");
+switch (gender) {
+  case "male":
+    gender = "Mr";
+    break;
+
+  case "female":
+    gender = "Miss";
+    break;
+
+  case "":
+    gender = "";
+    break;
+}
+
+var age = prompt("Age");
+
+if (age <= 0) {
+  alert("Wrong Input ...! " + "The Age Can Not Be " + age);
+}
+
+var welcomeAlert = confirm("Do You Wants To Continue The Welcoming Message..?");
+switch (welcomeAlert) {
+  case true:
+    alert("Welcome " + gender + " " + name);
+    break;
+
+  case false:
+    break;
+}
