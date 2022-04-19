2c5afb0b7a96e206f75a7c30dc88ac353ca9501b
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-04-19 14:37:14 +0300

    addTodoList

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-/* alert("Hello! I am an alert box!");
+alert("Hello! I am an alert box!");
 
-var name = prompt("Inter Your Name");
+let name = prompt("Inter Your Name");
+console.log(name);
 
-var gender = prompt("Inter Your Gender (male or female)");
+var gender = prompt("Inter Your Gender (male or female)").toLowerCase();
+console.log(gender);
+
 switch (gender) {
   case "male":
     gender = "Mr";
@@ -17,15 +20,16 @@ switch (gender) {
     break;
 }
 
-var age = prompt("Age");
-
+let age = prompt("Age");
+console.log(age);
 if (age <= 0) {
   alert("Wrong Input ...! ");
-  alert("The Age Can Not Be " + age);
+  alert("The Age Can Not Be " + "'" + age + "'");
   age;
 }
 
-var welcomeAlert = confirm("Do You Wants To Continue The Welcoming Message..?");
+let welcomeAlert = confirm("Do You Wants To Continue The Welcoming Message..?");
+console.log(welcomeAlert);
 switch (welcomeAlert) {
   case true:
     alert("Welcome " + gender + " " + name);
@@ -33,5 +37,34 @@ switch (welcomeAlert) {
 
   case false:
     break;
+
+  default:
+    break;
 }
- */
+
+let coffee = prompt("Do you want some Coffee?").toLowerCase();
+console.log(coffee);
+if (coffee === "") {
+  alert("invalid");
+} else if (coffee.toLocaleLowerCase === "yes") {
+  alert("Your Coffee In The Way " + gender + " " + name + "🎼");
+}
+
+let happy = prompt("Are you happy today?").toLowerCase();
+console.log(happy);
+if (happy === "") {
+  alert("invalid");
+} else if (happy === "yes") {
+  alert("It's Wonderful Day 🌻 ");
+}
+
+let meeting = prompt("Receiving interviews and meetings?").toLowerCase();
+console.log(meeting);
+if (meeting === "") {
+  alert("invalid");
+} else if (meeting === "yes") {
+  alert("A beautiful day with a beautiful start ✌ ");
+}
+
+let dailyArray = [coffee, happy, meeting];
+alert(dailyArray);
